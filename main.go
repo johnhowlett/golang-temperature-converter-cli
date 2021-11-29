983cd57b8e1c39ebab02338cc3e156907b3ad61e
John Howlett <john.howlett@mobi.ch>
2021-11-29 18:56:11 +0100

    Completed module2

--- main.go
+++ main.go
@@ -20,15 +20,29 @@ var errReadingInput = errors.New("Error reading input")
 func main() {
 	if len(os.Args) != 2 {
 		printError(errInvalidArguments)
-		os.Exit(1)
 	}
 	originUnit = strings.ToUpper(os.Args[1])
+
 	for {
 		fmt.Print("What is the current temperature in " + originUnit + " ? ")
+		_, err := fmt.Scanln(&originValue)
+		if err != nil {
+			printError(errReadingInput)
+		}
+
+		if originUnit == "C" {
+			convertToFahrenheit(originValue)
+		} else {
+			convertToCelsius(originValue)
+		}
 
 		fmt.Print("Would you like to convert another temperature ? (y/n) ")
+		_, err = fmt.Scanln(&shouldConvertAgain)
+		if err != nil {
+			printError(errReadingInput)
+		}
 
-		if shouldConvertAgain != "Y" {
+		if strings.ToUpper(strings.TrimSpace(shouldConvertAgain)) != "Y" {
 			fmt.Println("Good bye!")
 			break
 		}
